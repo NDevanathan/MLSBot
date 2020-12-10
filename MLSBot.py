6d8d22a20052a9e56e110285935eaa4d041232bd
Nikhil Devanathan <nikhil.devanathan@gmail.com>
2020-12-10 11:19:48 -0800

    12/10/20 - Basic cleaning
    Added no new functionalities, but cleaned up the overgrown IF tree. Still needs comments and server population, and dictionary reading.

--- MLSBot.py
+++ MLSBot.py
@@ -27,14 +27,38 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
-def getEmoji(content):
-    i = 0
-    
-def getServer(content):
-    i = 0
+async def handle_get_emoji(message):
+    mls = message.content[10:].replace(' gc','').replace(' ','')
+    if mls in MLS_TO_EMOJI:
+        await sendMsg(message, MLS_TO_EMOJI[mls])
+    else:
+        await sendMsg(message, "Unknown MLS abbreviation")
 
-def getSearch(content):
-    i = 0
+async def handle_get_server(message):
+    region = message.content[11:].replace(' gc','').replace(' ','')
+    if region in REGIONS_TO_SERVERS:
+        await sendMsg(message, REGIONS_TO_SERVERS[region])
+    else:
+        await sendMsg(message, "Unknown region")
+
+async def handle_get_search(message):
+    emote = message.content[11:].replace(' gc','').replace(' ','')
+    if emote in EMOJI_TO_REGION:
+        region = EMOJI_TO_REGION[emote]
+        if region in REGIONS_TO_SERVERS:
+            await sendMsg(message, emote + ', ' + region)
+            await sendMsg(message, REGIONS_TO_SERVERS[region])
+        else:
+            await sendMsg(message, "Unknown emoji")
+    else: 
+        await sendMsg(message, "Unknown emoji")
+
+async def sendMsg(msg_in, msg_out):
+    if ('gc' == msg_in.content[-2:]):
+        await msg_in.channel.send(msg_out)
+    else:
+        await msg_in.author.create_dm()
+        await msg_in.author.dm_channel.send(msg_out)
 
 @client.event
 async def on_ready():
@@ -44,77 +68,17 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
     if ('!mls' == message.content[:4]):
         if ('help' == message.content[5:9]):
-            if ('gc' == message.content[-2:]):
-                await message.channel.send(HELP_TEXT)
-            else:
-                await message.author.create_dm()
-                await message.author.dm_channel.send(HELP_TEXT)
+            await sendMsg(message, HELP_TEXT)
         elif ('emoji' == message.content[5:10]):
-            if ('gc' == message.content[-2:]):
-                mls = message.content[10:-2].replace(' ','')
-                if mls in MLS_TO_EMOJI:
-                    await message.channel.send(MLS_TO_EMOJI[mls])
-                else:
-                    await message.channel.send("Unknown MLS abbreviation")
-            else:
-                mls = message.content[10:].replace(' ','')
-                if mls in MLS_TO_EMOJI:
-                    await message.author.create_dm()
-                    await message.author.dm_channel.send(MLS_TO_EMOJI[mls])
-                else:
-                    await message.author.create_dm()
-                    await message.author.dm_channel.send("Unknown MLS abbreviation")
+            await handle_get_emoji(message)
         elif ('regions' == message.content[5:12]):
-            if ('gc' == message.content[-2:]):
-                await message.channel.send(REGIONS)
-            else:
-                await message.author.create_dm()
-                await message.author.dm_channel.send(REGIONS)
+            await sendMsg(message, REGIONS)
         elif ('server' == message.content[5:11]):
-            if ('gc' == message.content[-2:]):
-                region = message.content[11:-2].replace(' ','')
-                if region in REGIONS_TO_SERVERS:
-                    await message.channel.send(REGIONS_TO_SERVERS[region])
-                else:
-                    await message.channel.send("Unknown region")
-            else:
-                region = message.content[11:].replace(' ','')
-                if region in REGIONS_TO_SERVERS:
-                    await message.author.create_dm()
-                    await message.author.dm_channel.send(REGIONS_TO_SERVERS[region])
-                else:
-                    await message.author.create_dm()
-                    await message.author.dm_channel.send("Unknown region")
+            await handle_get_server(message)
         elif ('search' == message.content[5:11]):
-            if ('gc' == message.content[-2:]):
-                emote = message.content[11:-2].replace(' ','')
-                if emote in EMOJI_TO_REGION:
-                    region = EMOJI_TO_REGION[emote]
-                    if region in REGIONS_TO_SERVERS:
-                        await message.channel.send(emote + ', ' + region)
-                        await message.channel.send(REGIONS_TO_SERVERS[region])
-                    else:
-                        await message.channel.send("Unknown emoji")
-                else: 
-                    await message.channel.send("Unknown emoji")
-            else:
-                emote = message.content[11:].replace(' ','')
-                if emote in EMOJI_TO_REGION:
-                    region = EMOJI_TO_REGION[emote]
-                    if region in REGIONS_TO_SERVERS:
-                        await message.author.create_dm()
-                        await message.author.dm_channel.send(emote + ', ' + region)
-                        await message.author.dm_channel.send(REGIONS_TO_SERVERS[region])
-                    else:
-                        await message.author.create_dm()
-                        await message.author.dm_channel.send("Unknown emoji")
-                else:
-                    await message.author.create_dm()
-                    await message.author.dm_channel.send("Unknown emoji")
+            await handle_get_search(message)
         else:
             print(f'Recieved {message.system_content}')
             await message.channel.send("Unknown command")
